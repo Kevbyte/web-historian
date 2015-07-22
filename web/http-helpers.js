f90efdf1fb2857289f6267ecfa176eb514ee172b
Hack Reactor Students <accounts@hackreactor.com>
2015-07-22 16:55:45 -0700

    serve static file onto server

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
+var http = require('http');
 
 exports.headers = headers = {
   "access-control-allow-origin": "*",
@@ -14,6 +15,16 @@ exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
   // (Static files are things like html (yours or archived from others...),
   // css, or anything that doesn't change often.)
+
+  fs.readFile(asset, function (err, asset) {
+    if (err) {
+      res.writeHead(404);
+      res.end(JSON.stringify(err));
+      return;
+    }
+    res.writeHead(200);
+    res.end(asset);
+  });
 };
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,29 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var helpers = require('./http-helpers');
+var url = require('url');
 // require more modules/folders here!
 
+var asset = archive.paths.siteAssets + "/index.html";
+
+
+var actions = {
+  'GET': function(req, res){
+    helpers.serveAssets(res, asset);
+  },
+  'POST': function(req, res){
+    helpers.serveAssets(res, asset);
+  },
+  'OPTIONS': function(req, res){
+    helpers.serveAssets(res, asset);
+  }
+}
+
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = actions[req.method];
+  if(action){
+    action(req, res);
+  } else {
+    console.log('404');
+  }
 };
